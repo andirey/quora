c42417828bf77e2a4a9ff497700db0cfc156a9a2
and_rey <andirey@ip-172-31-40-214.eu-west-1.compute.internal>
2017-03-31 06:36:39 +0000

    Add "feather" read/write options

--- data_analysis.R
+++ data_analysis.R
@@ -4,13 +4,14 @@
 
 library(ggplot2)
 library(dplyr)
-library(h2o)
 library(tidytext)
-# library(data.table)
-library(dtplyr)
-library(topicmodels)
 library(randomForest)
 library(tm)
+library(data.table)
+library(dtplyr)
+library(feather)
+
+# library(topicmodels)
 
 
 # 1. Setups ---------------------------------------------------------------
@@ -20,30 +21,31 @@ path_data <- "../data/in/"
 fname_train_csv <- paste0(path_data, "train.csv")
 fname_test_csv <- paste0(path_data, "test.csv")
 
-# 2. Start H2O ------------------------------------------------------------
+fname_train_feather <- paste0(path_data, "train.feather")
+fname_test_feather <- paste0(path_data, "test.feather")
 
-# Run on http://127.0.0.1:54321/
-localH2O = h2o.init()
-h2o.init()
+# 2. Read data ------------------------------------------------------------
 
-# 3. Read data ------------------------------------------------------------
-
-# 3.1. Train data set
-if(file.exists(fname_train_csv)){
-  df_train <- h2o.importFile(fname_train_csv) %>% as.data.frame()
-  # df_train <- read.csv(fname_train_csv)
+# 2.1. Train data set
+if(!file.exists(fname_train_feather)){
+  df_train <- read.csv(fname_train_csv)
+  write_feather(df_train, fname_train_feather)
+} else {
+  df_train <- read_feather(fname_train_feather)
 }
 
-# 3.2. Test data set
-if(file.exists(fname_train_csv)){
-  df_test <- h2o.importFile(fname_test_csv) %>% as.data.frame()
-  # df_test <- read.csv(fname_test_csv)
+# 2.2. Test data set
+if(!file.exists(fname_test_feather)){
+  df_test <- read.csv(fname_test_csv)
+  write_feather(df_test, fname_test_feather)
+} else {
+  df_test <- read_feather(fname_test_feather)
 }
 
 # 4. Feature Engineering --------------------------------------------------
 
 # 4.1. Change column names
-colnames(df_train) <- c("id", "id_q1", "id_q2", "q1", "q2", "is_dupl")
+colnames(df_train) <- c("id", "id_q1", "id_q2", "q1", "q2", "is_duplicate")
 colnames(df_test) <- c("id", "q1", "q2")
 
 # 4.2. Clean up data
@@ -88,4 +90,10 @@ head(df_test,3)
 
 
 
+# H2O ---------------------------------------------------------------------
 
+# library(h2o)
+# Run on http://127.0.0.1:54321/
+# localH2O = h2o.init()
+# h2o.init()
+# df_train <- h2o.importFile(fname_train_csv) %>% as.data.frame()
