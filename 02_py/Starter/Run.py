9dff0aee49b508c8570159e19cd140a986fdc563
andrii <happydatascientist@gmail.com>
2017-04-04 16:11:20 +0300

    Add reading training data set and quick analysis

--- 02_py/Starter/Run.py
+++ 02_py/Starter/Run.py
@@ -17,3 +17,25 @@ print('# File sizes')
 for f in os.listdir('../../data/in/'):
     if 'zip' not in f:
         print(f.ljust(30) + str(round(os.path.getsize('../../data/in/' + f) / 1000000, 2)) + 'MB')
+
+# Read data
+df_train = pd.read_csv('../../data/in/train.csv')
+print(df_train.head())
+
+# Quick analysis of train data set
+print('Total number of question pairs for training: {}'.format(len(df_train)))
+print('Duplicate pairs: {}%'.format(round(df_train['is_duplicate'].mean()*100, 2)))
+
+qids = pd.Series(df_train['qid1'].tolist() + df_train['qid2'].tolist())
+print('Total number of questions in the training data: {}'.format(len(np.unique(qids))))
+print('Number of questions that appear multiple times: {}'.format(np.sum(qids.value_counts() > 1)))
+
+# Histogram
+plt.figure(figsize=(12, 5))
+plt.hist(qids.value_counts(), bins=50)
+plt.yscale('log', nonposy='clip')
+plt.title('Log-Histogram of question appearance counts')
+plt.xlabel('Number of occurences of question')
+plt.ylabel('Number of questions')
+print()
+
